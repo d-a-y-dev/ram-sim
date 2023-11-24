bac76592c4f2c39bf738a5161d62b7908f6b8679
Suphawit <suphawit.won@gmail.com>
2023-11-24 22:06:25 +0700

    implemented second working version

--- src/dram.cpp
+++ src/dram.cpp
@@ -27,11 +27,19 @@ void inspect_dram(uint32_t start_address, uint32_t end_address){
 }
 
 uint32_t get_block_data_at_address(cache_block block){
-    uint32_t first_part = (block.data[0] & 0xFF);
-    uint32_t second_part = (block.data[1]  & 0xFF) << 8;
-    uint32_t third_part = (block.data[2] & 0xFF) << 16;
-    uint32_t fourth_part = (block.data[3] & 0xFF) << 24;
-    uint32_t return_data = fourth_part |
+    uint32_t first_part = (block.data[0] & 0xF);
+    uint32_t second_part = (block.data[1]  & 0xF) << 4;
+    uint32_t third_part = (block.data[2] & 0xF) << 8;
+    uint32_t fourth_part = (block.data[3] & 0xF) << 12;
+    uint32_t fifth_part = (block.data[4] & 0xF) << 16;
+    uint32_t sixth_part = (block.data[5] & 0xF) << 20;
+    uint32_t seventh_part = (block.data[6] & 0xF) << 24;
+    uint32_t eigth_part = (block.data[7] & 0xF) << 28;
+    uint32_t return_data = eigth_part |
+        seventh_part |
+        sixth_part |
+        fifth_part |
+        fourth_part |
         third_part |
         second_part |
         first_part;
@@ -72,7 +80,8 @@ void init_dram(int chip_per_rank, int bank_per_chip, int row_size, int col_size)
 /***************************************************************/
 uint32_t read_dram(uint32_t address) {                                           
     printf("READ\n");
-    uint32_t col_id = address & 0x3FF;
+    uint32_t chip_id = address & 0x7;
+    uint32_t col_id = (address >> 3) & 0x3FF;
     uint32_t row_id = (address >> 10) & 0x3FF;
     uint32_t bank_id = (address >> 20) & 0x7;
     cache_block block;
@@ -82,11 +91,8 @@ uint32_t read_dram(uint32_t address) {
             (dram.chips[i].banks[bank_id].cells[row_id][col_id+1] <<  8) |
             (dram.chips[i].banks[bank_id].cells[row_id][col_id] <<  0));
     }
-    for(int i = 0; i < 8; i++){
-        printf("%8x\n", block.data[i]);
-    }
     uint32_t data = get_block_data_at_address(block);
-    return data;
+    return data >> (chip_id * 8);
 }
 
 /***************************************************************/
@@ -98,28 +104,12 @@ uint32_t read_dram(uint32_t address) {
 /***************************************************************/
 void write_dram(uint32_t address, uint32_t value) {                                           
     printf("WRITE\n");
-    uint32_t col_id = address & 0x3FF;
+    uint32_t col_id = (address >> 3) & 0x3FF;
     uint32_t row_id = (address >> 10) & 0x3FF;
     uint32_t bank_id = (address >> 20) & 0x7;
     for (int i = 0; i < 8; i++) {
-            dram.chips[i].banks[bank_id].cells[row_id][col_id+3] = value >> 24 & 0xFF;
-            dram.chips[i].banks[bank_id].cells[row_id][col_id+2] = value >> 16 & 0xFF;
-            dram.chips[i].banks[bank_id].cells[row_id][col_id+1] = value >> 8 & 0xFF;
-            dram.chips[i].banks[bank_id].cells[row_id][col_id] = value & 0xFF;
-            value = value >> 8;
+            dram.chips[i].banks[bank_id].cells[row_id][col_id] = value & 0xF;
+            value = value >> 4;
     }
 }
 
-/*
-int main(void){
-    init_dram(8, 8, 1024, 1024);
-    cache_block block = read_dram(0x00000000);
-    uint32_t data = get_block_data_at_address(block);
-    printf("data: %x\n", data);
-    write_dram(0x00000000, 0xFFFFFFFF);                                         
-    block = read_dram(0x00000000);
-    data = get_block_data_at_address(block);
-    printf("data: %x\n", data);
-    return 0;
-}
-*/
